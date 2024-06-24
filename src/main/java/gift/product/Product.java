07163c239dba9a00694d1b71e4b44afb7391176a
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-06-24 20:15:36 +0900

    feat : Product record Product 레코드

--- src/main/java/gift/product/Product.java
+++ src/main/java/gift/product/Product.java
@@ -1,21 +1,5 @@
-package product;
+package gift.product;
 
-public class Product
-        //(Long id, String name, Long price, String imageUrl) {
-{
+public record Product(Long id, String name, Long price, String imageUrl) {
 
-    Long id;
-    String name;
-    Long price;
-    String imageUrl;
-
-    public Product(Long id, String name, Long price, String imageUrl) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.imageUrl = imageUrl;
-    }
-
-    public Product() {
-    }
 }
