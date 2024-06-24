738fbb793630702e96403327ad20048c6c70b021
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-06-24 20:17:27 +0900

    feat : ProductController addProduct method 상품 추가 API 구현

--- src/main/java/gift/product/ProductController.java
+++ src/main/java/gift/product/ProductController.java
@@ -1,5 +1,6 @@
-package product;
+package gift.product;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -8,44 +9,16 @@ import java.util.*;
 public class ProductController {
     private final Map<Long, Product> products = new HashMap<>();
 
-    @GetMapping("/products")
-    public Collection<Product> getAllProducts(){
-        System.out.println("getAll");
-        return products.values();
-    }
-    @GetMapping("/product/get")
-    public Product getProduct(@RequestParam("id") Long id){
-        System.out.println("get");
-        return products.get(id);
-    }
-
     @PostMapping("/product/add")
     public Product addProduct(@RequestParam("id") Long id,
                               @RequestParam("name") String name,
                               @RequestParam("price") Long price,
                               @RequestParam("imageUrl") String imageUrl){
         System.out.println("add");
-        return products.put(id, new Product(id, name, price, imageUrl));
+        Product product = new Product(id, name, price, imageUrl);
+        products.put(id, product);
+        return product;
     }
 
-    @PostMapping("/product/update")
-    public Product updateProduct(@RequestParam("id") Long id,
-                                 @RequestParam("name") String name,
-                                 @RequestParam("price") Long price,
-                                 @RequestParam("imageUrl") String imageUrl){
-        System.out.println("update");
-        if(products.get(id) != null){
-            return products.put(id, new Product(id, name, price, imageUrl));
-        }
-        else return null;
-    }
 
-    @DeleteMapping("/product/delete")
-    public Product deleteProduct(@RequestParam("id") Long id){
-        System.out.println("delete");
-        if(products.get(id) != null){
-            return products.remove(id);
-        }
-        return null;
-    }
 }
