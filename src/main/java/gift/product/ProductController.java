448c7412fc080fc550ce3b90fcd2e663ee843098
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-06-28 14:52:05 +0900

    fix : Product Controller HashMap 삭제 후, Repository를 사용하는 로직으로 변경

--- src/main/java/gift/product/ProductController.java
+++ src/main/java/gift/product/ProductController.java
@@ -1,4 +1,5 @@
 package gift.product;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,47 +8,44 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Controller
 public class ProductController {
-    private final Map<Long, Product> products = new HashMap<>();
+    private final ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
     @PostMapping("/manager/product/add")
     public String addProduct(@ModelAttribute Product product, RedirectAttributes redirectAttributes){
         System.out.println("add");
-        products.put(product.id(), product);
-        redirectAttributes.addAttribute("id", product.id());
-        System.out.println(product.id());
+        Product product1 = productRepository.insertProduct(product);
+        redirectAttributes.addAttribute("id", product1.id());
+        System.out.println(product1.id());
         return "redirect:/manager/product/{id}";
     }
 
     @PostMapping("/manager/product/update/{id}")
-    public String updateProduct(@ModelAttribute Product product, RedirectAttributes redirectAttributes){
+    public String updateProduct(@PathVariable Long id, @ModelAttribute Product product, RedirectAttributes redirectAttributes){
         System.out.println("update");
-        products.put(product.id(), product);
-        redirectAttributes.addAttribute("id", product.id());
+        productRepository.updateProduct(id, product);
+        redirectAttributes.addAttribute("id", id);
         return "redirect:/manager/product/{id}";
     }
 
     @PostMapping("/manager/product/delete/{id}")
     public String deleteProduct(@PathVariable Long id){
         System.out.println("delete");
-        Product product = products.get(id);
+        Product product = productRepository.selectProduct(id);
         if(product != null){
-            products.remove(id);
+            productRepository.deleteProduct(id);
         }
         return "redirect:/manager/products";
     }
 
     @GetMapping("/manager/products")
     public String getProductsView(Model model){
-        model.addAttribute("products", products.values());
-        for (Product value : products.values()) {
-            System.out.println(value.name());
-
-        }
+        model.addAttribute("products", productRepository.selectProducts());
         return "ManageProduct";
     }
 
@@ -59,13 +57,13 @@ public class ProductController {
 
     @GetMapping("/manager/product/update/{id}")
     public String updateProductView(@PathVariable Long id, Model model){
-        model.addAttribute("product", products.get(id));
+        model.addAttribute("product", productRepository.selectProduct(id));
         return "AddOrUpdateProduct";
     }
 
     @GetMapping("/manager/product/{id}")
     public String getProduct(@PathVariable long id, Model model) {
-        Product product = products.get(id);
+        Product product = productRepository.selectProduct(id);
         model.addAttribute("product", product);
         return "ProductInfo";
     }
